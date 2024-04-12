b432d2a095ca3a37aa6ac433d5a7be5faf72e631
wns2619 <130582634+wns2619@users.noreply.github.com>
2024-04-12 15:10:45 +0900

    Binary Search Tree

---
+++ DataStructues/AVL.h
@@ -0,0 +1,188 @@
+#pragma once
+
+#include "BinarySearchTree.h"
+
+template<typename K, typename V>
+class AVL : public BinarySearchTree<K, V>
+{
+public:
+	using Base = BinarySearchTree<K, V>;
+	using Item = BinarySearchTree<K, V>::Item;
+	using Node = BinarySearchTree<K, V>::Node;
+
+	//struct Item {
+	//	K key = K();	// first
+	//	V value = V();	// second
+	//};
+
+	//struct Node {
+	//	Item item;
+	//	Node* left = nullptr;
+	//	Node* right = nullptr;
+	//};
+
+	int Height(Node* node)
+	{
+		return Base::Height(node); // 헷갈림 방지용
+	}
+
+	int Balance(Node* node)
+	{
+		if (node)
+			return Base::Height(node->left) - Base::Height(node->right);
+		else
+			return 0;
+	}
+
+	Node* RotateLeft(Node* parent)
+	{
+		// TODO:
+		Node* temp = parent->right;
+		parent->right = temp->left;
+		temp->left = parent;
+
+		return temp;
+	}
+
+	Node* RotateRight(Node* parent)
+	{
+		// TODO:
+		Node* temp = parent->left;
+		parent->left = temp->right;
+		temp->right = parent;
+
+		return temp;
+	}
+
+	void Insert(const Item& item)
+	{
+		root_ = Insert(root_, item);
+	}
+
+	Node* Insert(Node* node, const Item& item)
+	{
+		if (!node)
+			return new Node{ item, nullptr, nullptr };
+
+		const auto& key = item.key;
+
+		if (key < node->item.key)
+			node->left = Insert(node->left, item);
+		else if (key > node->item.key)
+			node->right = Insert(node->right, item);
+		else
+		{
+			node->item = item;
+			return node;
+		}
+
+		int balance = Balance(node);
+
+		// balance가 0, 1, -1 이면 조절할 필요가 없다고 판단
+
+		// LL -> Right Rotation
+		if (balance > 1 && Balance(node->left) >= 0)
+			return RotateRight(node);
+
+		//	TODO:
+
+		// RR -> Left Rotation
+		if (balance < -1 && Balance(node->right) <= 0)
+			return RotateLeft(node);
+
+		// LR -> Left-Right Rotation
+		if (balance > 1 && Balance(node->left) < 0)
+		{
+			node->left = RotateLeft(node->left);
+			return RotateRight(node);
+		}
+
+		// RL -> Right-Left Rotation
+		if (balance < -1 && Balance(node->right) > 0)
+		{
+			node->right = RotateRight(node->right);
+			return RotateLeft(node);
+		}
+
+		return node;
+	}
+
+	Node* MinKeyNode(Node* node)
+	{
+		assert(node);
+		while (node->left)
+			node = node->left;
+		return node;
+	}
+
+	void Remove(const K& key)
+	{
+		using namespace std;
+		cout << "Remove " << key << endl;
+		root_ = Remove(root_, key);
+	}
+
+	Node* Remove(Node* node, const K& key)
+	{
+		// BST와 동일
+		if (!node) return node;
+
+		if (key < node->item.key)
+			node->left = Remove(node->left, key);
+		else if (key > node->item.key)
+			node->right = Remove(node->right, key);
+		else
+		{
+			if (!node->left)
+			{
+				Node* temp = node->right;
+				delete node;
+				return temp;
+			}
+			else if (!node->right)
+			{
+				Node* temp = node->left;
+				delete node;
+				return temp;
+			}
+
+			Node* temp = MinKeyNode(node->right);
+			node->item = temp->item;
+			node->right = Remove(node->right, temp->item.key);
+		}
+
+		if (node == NULL)
+			return node;
+
+		// 균형 잡기
+		int balance = Balance(node);
+
+		// LL -> Right Rotation
+		if (balance > 1 && Balance(node->left) >= 0)
+			return RotateRight(node);
+
+		// RR -> Left Rotation
+		if (balance < -1 && Balance(node->right) <= 0)
+			return RotateLeft(node);
+
+		// LR -> Left-Right Rotation
+		if (balance > 1 && Balance(node->left) < 0)
+		{
+			node->left = RotateLeft(node->left);
+			return RotateRight(node);
+		}
+
+		// RL -> Right-Left Rotation
+		if (balance < -1 && Balance(node->right) > 0)
+		{
+			node->right = RotateRight(node->right);
+			return RotateLeft(node);
+		}
+
+		return node;
+	}
+
+private:
+	Node*& root_ = BinarySearchTree<K, V>::root_;
+	// this->root_로도 사용 가능
+};
--- DataStructues/DataStructues.cpp
+++ DataStructues/DataStructues.cpp
@@ -1,314 +1,35 @@
-﻿// DataStructues.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-#define _CRT_SECURE_NO_WARNINGS
+﻿#include <iostream>
 
-#include "Stack.h"
-#include "Queue.h"
-#include "Deque.h"
-#include "SinglyLinkedList.h"
-#include "DoublyLinkedList.h"
-#include "BinaryTree.h"
+#include "AVL.h"
 
 using namespace std;
 
-
-// Function to return precedence of operators
-int Prec(char c);
-void InfixToPostfix(Queue<char>& q, Queue<char>& output);
-int EvalPostfix(Queue<char>& q);
-
-class ExpressionTree : public BinaryTree<char>
-{
-public:
-	typedef BinaryTree<char> Base;
-
-	ExpressionTree(Node* root) : Base(root) {}
-
-	bool IsDigit(char c)
-	{
-		if (c >= '0' && c <= '9') return true;
-		else return false;
-	}
-
-	int Evaluate()
-	{
-		return Evaluate(Base::root_);
-	}
-
-	int Evaluate(Node* node)
-	{
-		// TODO: 트리에 저장된 수식의 결과값을 계산
-		if (nullptr == node) return 0;
-
-		if (node->item >= '0' && node->item <= '9')
-			return node->item - '0';
-		else if (node->item == '*')
-			return Evaluate(node->left) * Evaluate(node->right);
-		else if (node->item == '-')
-			return Evaluate(node->left) - Evaluate(node->right);
-		else if (node->item == '+')
-			return Evaluate(node->left) + Evaluate(node->right);
-		else
-			return Evaluate(node->left) / Evaluate(node->right);
-
-		return 0;
-	}
-
-	void Infix() { Infix(root_); cout << endl; }
-	void Infix(Node* node) {
-		// TODO: 수식을 Infix 형식으로 출력 (괄호 포함)
-		 // (5+((3-2)*4)) <- 출력 예시
-		if (node != nullptr)
-		{
-			if (!IsDigit(node->item)) std::cout << "(";
-			Infix(node->left);
-			cout << node->item;
-			Infix(node->right);
-			if (!IsDigit(node->item)) cout << ")";
-		}
-	}
-
-	void Postfix() { Postfix(root_);  cout << endl; }
-	void Postfix(Node* node) {
-		// TODO: 수식을 Postfix 형식으로 출력
-		 // 532-4*+ <- 출력 예시
-		if (node != nullptr)
-		{
-			Postfix(node->left);
-			Postfix(node->right);
-			cout << node->item;
-		}
-	}
-
-	// Infix -> postfix -> expression tree
-	ExpressionTree(const char* infix)
-	{
-		
-		Queue<char> q;
-		for (int i = 0; infix[i] != '\0'; i++)
-			q.Enqueue(infix[i]);
-		cout << "  Infix: ";
-		q.Print();
-		Queue<char> postfix;
-		InfixToPostfix(q, postfix);
-		cout << "Postfix: ";
-		postfix.Print();
-
-		// Postfix -> Expression tree
-
-		Stack<Node*> s;
-
-		while (!postfix.IsEmpty())
-		{
-			char c = postfix.Front();
-			postfix.Dequeue();
-
-			// Infix -> Postfix (예제 재사용)
-			// (5+((3-2)*4)) ->  // 532-4*+ <- 출력 예시
-			if (c >= '0' && c <= '9')
-			{
-				// TODO:
-				Node* temp = new Node{ c, nullptr, nullptr };
-				s.Push(temp);
-			}
-			else
-			{
-				Node* right = s.Top();
-				s.Pop();
-				Node* left = s.Top();
-				s.Pop();
-
-				Node* temp = new Node{ c, left, right };
-				s.Push(temp);
-			}
-		}
-
-		root_ = s.Top();
-	}
-};
-
 int main()
 {
-	using Node = ExpressionTree::Node;
+	using Item = AVL<int, char>::Item;
 
-	// 5 + (3 - 2) * 4
-	// 
-	//    +
-	//	/   \
-	// 5     *
-	//	    / \
-	//	   -   4
-	//	  / \
-	//	 3   2
+	AVL<int, char> bst;
 
-	Node* n1 = new Node{ '5', nullptr, nullptr };
-	Node* n2 = new Node{ '+', nullptr, nullptr };
-	Node* n3 = new Node{ '3', nullptr, nullptr };
-	Node* n4 = new Node{ '-', nullptr, nullptr };
-	Node* n5 = new Node{ '2', nullptr, nullptr };
-	Node* n6 = new Node{ '*', nullptr, nullptr };
-	Node* n7 = new Node{ '4', nullptr, nullptr };
-
-	// +
-	n2->left = n1; // 5
-	n2->right = n6; // *
-
-	// *
-	n6->left = n4; // -
-	n6->right = n7; // 4
-
-	// -
-	n4->left = n3; // 3
-	n4->right = n5; // 2
-
-	ExpressionTree tree(n2);
-
-	tree.Print2D();
-
-	// 수식 트리에 저장되어 있는 수식을 실제로 계산해서 그 결과를 출력합니다.
-	cout << "Evaluated = " << tree.Evaluate() << endl; // Evaluated = 9
-
-	/// 수식 트리에 저장되어 있는 수식을 Infix 방식으로 출력합니다.
-	cout << "  Infix: ";
-	tree.Infix();   // (5+((3-2)*4)) <- 출력 예시
-
-	// 수식 트리에 저장되어 있는 수식을 Postfix 방식으로 출력합니다.
-	cout << "Postfix: ";
-	tree.Postfix(); // 532-4*+ <- 출력 예시
-
-	cout << endl;
-
-	// Infix -> Postfix -> Expression Tree
+	for (int i : { 9, 8, 7, 6, 5, 4, 3, 2, 1 })
+		//for (int i : { 1, 2, 3, 4, 5, 6, 7, 8, 9 })
 	{
-		// const char infix[] = "1+(1*2+3)*4";
-		const char infix[] = "(5+((3-2)*4))";
+		bst.Insert(Item{ i, char('A' + i) });
+		bst.Print2D();
+	}
 
-		// 생성자에서 문자열로 입력받은 수식을 내부적으로 Infix->Postfix 과정을 거쳐 트리로 저장합니다.
-		ExpressionTree tree(infix);
+	bst.Inorder(); // key에 대해 정렬
 
-		tree.Print2D();
+	cout << bst.RecurGet(5)->value << endl;
+	cout << bst.RecurGet(3)->value << endl;
+	cout << bst.RecurGet(7)->value << endl;
+	cout << bst.RecurGet(1)->value << endl;
+	cout << bst.RecurGet(4)->value << endl;
 
-		// 수식 트리에 저장되어 있는 수식을 실제로 계산해서 그 결과를 출력합니다.
-		cout << "Evaluated = " << tree.Evaluate() << endl; // Evaluated = 9
+	for (int i : {5, 4, 7, 8, 9})
+	{
+		bst.Remove(i);
+		bst.Print2D();
 	}
 
 	return 0;
-}
-
-// Function to return precedence of operators
-int Prec(char c)
-{
-	if (c == '/' || c == '*')
-		return 2;
-	else if (c == '+' || c == '-')
-		return 1;
-	else
-		return -1; // '('는 우선순위가 아주 낮은 것으로 처리
-}
-
-void InfixToPostfix(Queue<char>& q, Queue<char>& output)
-{
-	Stack<char> s; // 우선순위가 낮은 연산을 보류하기 위한 스택
-
-	output.SetDebugFlag(false);
-
-	while (!q.IsEmpty())
-	{
-		char c = q.Front();
-		q.Dequeue();
-
-		//cout << c << endl;
-
-		if (c >= '0' && c <= '9') // 숫자(피연산자)라면 output에 추가
-			output.Enqueue(c);
-		else if (c == '(') // 여는 괄호라면 스택에 추가
-			s.Push(c);
-		else if (c == ')') // 닫는 괄호를 만나면
-		{
-			// 여는 괄호 전까지를 스택에서 꺼내서 출력에 넣기
-			while (s.Top() != '(')
-			{
-				output.Enqueue(s.Top());
-				s.Pop();
-			}
-			s.Pop(); // 여는 괄호 제거
-		}
-		else // 연산자를 만나면
-		{
-			// 스택에서 c보다 우선순위가 높거나 같은 것들을 꺼내서 추가
-			while (!s.IsEmpty() && Prec(c) <= Prec(s.Top()))
-			{
-				output.Enqueue(s.Top());
-				s.Pop();
-			}
-
-			// c는 스택에 추가
-			s.Push(c);
-		}
-
-		//cout << "Stack: ";
-		//s.Print();
-		//cout << "Output:";
-		//output.Print();
-		//cout << endl;
-	}
-
-	// 스택에 남아있는 것들을 모두 추가
-	while (!s.IsEmpty())
-	{
-		output.Enqueue(s.Top());
-		s.Pop();
-	}
-}
-
-int EvalPostfix(Queue<char>& q)
-{
-	Stack<int> s;
-
-	while (!q.IsEmpty())
-	{
-		char c = q.Front();
-		q.Dequeue();
-
-		//cout << c << endl;
-
-		if (c != '+' && c != '-' && c != '*' && c != '/')
-		{
-			s.Push(c - '0'); // 입력이 연산자가 아니면 일단 저장
-		}
-		else
-		{
-			//cout << "Operator: " << c << endl;
-
-			// 입력이 연산자이면 스택에서 꺼내서 연산에 사용
-			int op2 = s.Top(); // op1, op2 순서 주의
-			s.Pop();
-			int op1 = s.Top();
-			s.Pop();
-
-			if (c == '+') {
-				s.Push(op1 + op2);
-			}
-			else if (c == '-') {
-				s.Push(op1 - op2);
-			}
-			else if (c == '*') {
-				s.Push(op1 * op2);
-			}
-			else if (c == '/')
-			{
-				s.Push(op1 / op2);
-			}
-			else
-			{
-				cout << "Wrong operator" << endl;
-				exit(-1); // 강제 종료
-			}
-		}
-
-		//cout << "Stack: ";
-		//s.Print();
-	}
-
-	return s.Top();
 }
